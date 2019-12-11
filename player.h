b274a7bcd608418ffdd1c8a27a9ea8e73d254d18
Abrclam <57803369+Abrclam@users.noreply.github.com>
2019-12-11 08:59:26 -0700

    Create player.h

---
+++ player.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "globals.h"
+#include "GameObject.h"
+
+class player : public gameObject {
+private:
+	double xVel;
+	double yVel;
+	bool CanJump;
+	bool OnLadder;
+
+public:
+	player();
+	void move(vector<gameObject*>stuff);
+	void moveLeft();
+	void moveRight();
+	void climb();
+	void Jump();
+	void draw();
+	int VerticalCollision(int x1, int y1, int w1, int h1, int x2, int y2, int w2, int h2, int ydir);
+	int LadderCollision(int x1, int y1, int w1, int h1, int x2, int y2, int w2, int h2);
+
+};
